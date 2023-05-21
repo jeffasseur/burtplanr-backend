7e86f608fa980cf57fffb55aadaf7a157f1c6dd0
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-05-21 18:06:32 +0200

    feat: app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ try {
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3002', 'http://localhost:3000']
+  origin: ['http://localhost:3002', 'http://localhost:3000', 'http://127.0.0.1:3002', 'http://127.0.0.1:3000', 'http://0.0.0.0']
 }
 app.use(cors(corsOptions));
 

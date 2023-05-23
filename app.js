bffc0068a4741a43dd61e616214c54342eb76696
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-05-23 09:23:06 +0200

    feat: change burger model

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const MONGODB = process.env.MONGO_DB;
+const MONGODB = 'mongodb://127.0.0.1:27017/buurtplanr';
 // const production = require('./config/production.json');
 
 
